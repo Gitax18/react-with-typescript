8d5784763d467bf55a463fbb0a5715257f140dc2
Gitax18 <lusifer0418@gmail.com>
2025-03-04 00:44:11 +0530

    comment state component 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // import Parent from "./props/Parent";
 
-import GuestList from "./states/GuestList";
+// import GuestList from "./states/GuestList";
 
 function App() {
   return (
     <>
       {/* <h1>Hello World</h1> */}
       {/* <Parent /> */}
-      <GuestList />
+      {/* <GuestList /> */}
     </>
   );
 }
